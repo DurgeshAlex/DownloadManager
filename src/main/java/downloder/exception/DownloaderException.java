d3df81bb31862e6e2aae03009c4dd5f1fa123a16
TechnicalGuru <durgeshkumarcse0051@gmail.com>
2019-02-03 00:38:22 +0530

    Revert "Basic multithreaded download manager"
    This reverts commit 4e8f8f28fad82ca99ba47c678ae8b069d9c176b0.

--- src/main/java/downloder/exception/DownloaderException.java
+++
@@ -1,9 +0,0 @@
-package downloder.exception;
-
-public class DownloaderException extends Exception {
-	public static final String NOTHINGTORESUME ="Nothing to RESUME.File already downloaded";
-	public DownloaderException(String msg) {
-		super(msg);
-	}
-
-}
